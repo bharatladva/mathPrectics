c38e3850ffe6ba8f45ab53eda013c17f38d593cb
bharatladva <bharatladva77@gmail.com>
2024-03-15 08:38:57 +0530

    add input

--- src/components/MetricsGrid/MetricsGrid.jsx
+++ src/components/MetricsGrid/MetricsGrid.jsx
@@ -39,6 +39,47 @@ const MetricsGrid = () => {
 		setShowColSum(!showColSum);
 	};
 
+	//-----------------------------------------------------------------
+
+	const [inputs, setInputs] = useState(Array(9).fill(""));
+	const [colInputs, setColInputs] = useState(Array(9).fill(""));
+
+	const handleInputChange = (index, event) => {
+		const newValue = event.target.value;
+		// Check if the input is a valid integer
+		if (/^\d*$/.test(newValue)) {
+			const newInputs = [...inputs];
+			newInputs[index] = newValue;
+			setInputs(newInputs);
+		}
+	};
+
+	const handleColInputChange = (index, event) => {
+		const newValue = event.target.value;
+		// Check if the input is a valid integer
+		if (/^\d*$/.test(newValue)) {
+			const newInputs = [...colInputs];
+			newInputs[index] = newValue;
+			setColInputs(newInputs);
+		}
+	};
+
+	const handleKeyDown = (index, event) => {
+		if (event.key === "ArrowRight" && index < 8) {
+			document.getElementById(`input-${index + 1}`).focus();
+		} else if (event.key === "ArrowLeft" && index > 0) {
+			document.getElementById(`input-${index - 1}`).focus();
+		}
+	};
+	const handleColKeyDown = (index, event) => {
+		if (event.key === "ArrowRight" && index < 8) {
+			document.getElementById(`colinput-${index + 1}`).focus();
+		} else if (event.key === "ArrowLeft" && index > 0) {
+			document.getElementById(`colinput-${index - 1}`).focus();
+		}
+	};
+	//--------------------------------------------------------------
+
 	return (
 		<div className='container'>
 			<div className='main'>
@@ -77,6 +118,36 @@ const MetricsGrid = () => {
 						</div>
 					)}
 				</div>
+
+				<div className='userInput'>
+					<div className='nine-input-box'>
+						<h2>Enter your row sum</h2>
+						{inputs.map((value, index) => (
+							<input
+								key={index}
+								id={`input-${index}`}
+								className='input'
+								value={value}
+								onChange={(event) => handleInputChange(index, event)}
+								onKeyDown={(event) => handleKeyDown(index, event)}
+							/>
+						))}
+					</div>
+					<div className='nine-input-box'>
+						<h2>Enter your col sum</h2>
+						{colInputs.map((value, index) => (
+							<input
+								key={index}
+								id={`colinput-${index}`}
+								className='input'
+								value={value}
+								onChange={(event) => handleColInputChange(index, event)}
+								onKeyDown={(event) => handleColKeyDown(index, event)}
+							/>
+						))}
+					</div>
+				</div>
+
 				<div className='buttons-div'>
 					<button onClick={randomizeMetrics}>Randomize Table</button>
 					<button onClick={toggleRowSum}>
