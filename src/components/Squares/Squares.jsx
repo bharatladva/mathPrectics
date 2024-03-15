fc29422c955516ed2ea811c424f94641b384814c
bharatladva <bharatladva77@gmail.com>
2024-03-15 09:17:41 +0530

    add cubes squres

--- src/components/Squares/Squares.jsx
+++ src/components/Squares/Squares.jsx
@@ -1,9 +1,22 @@
 /** @format */
-
+import "./styles.css";
 export default function Squares() {
+	const powers = [];
+	for (let i = 1; i <= 40; i++) {
+		powers.push(
+			<div
+				key={i}
+				className='power'
+			>
+				{i}
+				<sup>{2}</sup> = {Math.pow(i, 2)}
+			</div>
+		);
+	}
+
 	return (
-		<div className='containar'>
-			<div>squares</div>
+		<div className='container'>
+			<div className='powers-container'>{powers}</div>
 		</div>
 	);
 }
--- src/components/cubes/Cubes.jsx
+++ src/components/cubes/Cubes.jsx
@@ -1,7 +1,25 @@
 /** @format */
 
 import React from "react";
+import "./styles.css";
 
 export default function Cubes() {
-	return <div>Cubes</div>;
+	const cubes = [];
+	for (let i = 1; i <= 20; i++) {
+		cubes.push(
+			<div
+				key={i}
+				className='cube'
+			>
+				{i}
+				<sup>3</sup> = {Math.pow(i, 3)}
+			</div>
+		);
+	}
+
+	return (
+		<div className='container'>
+			<div className='cubes-container'>{cubes}</div>
+		</div>
+	);
 }
