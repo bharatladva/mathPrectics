a0180c7f1403ea89812740213dd6362dcf9dbf63
bharatladva <bharatladva77@gmail.com>
2024-04-02 10:54:35 +0530

    fix table style

--- src/components/tables/Tables.jsx
+++ src/components/tables/Tables.jsx
@@ -1,26 +1,46 @@
 /** @format */
-import "./styles.css";
-export default function tables() {
-	const numbers = Array.from({ length: 10 }, (_, index) => index + 1);
+
+import { useState } from "react";
+import "./styles.css"; // Import CSS file for styling
+
+export default function Tables() {
+	const numbers = [...Array(10).keys()].map((i) => i + 1); // Generate numbers 1 to 10
+	const multiplicationNumbers = [...Array(29).keys()].map((i) => i + 2); // Generate numbers 2 to 30
+	const [hoveredCell, setHoveredCell] = useState(null);
 
 	return (
-		<div className='multiplication-chart'>
-			<table>
-				<thead>
-					<tr></tr>
-				</thead>
-				<tbody>
-					{numbers.map((rowNum) => (
-						<tr key={rowNum}>
-							{numbers.map((colNum) => (
-								<td key={`${rowNum}-${colNum}`}>{`${rowNum} x ${colNum} = ${
-									rowNum * colNum
-								}`}</td>
+		<div className='multiplication-table'>
+			{multiplicationNumbers.map((multiplier) => (
+				<div
+					key={multiplier}
+					className='table-container'
+				>
+					<h3>{multiplier}'s Table</h3>
+					<table>
+						<thead></thead>
+						<tbody>
+							{numbers.map((num) => (
+								<tr key={num}>
+									<td>
+										{multiplier} x {num} =
+									</td>
+									<td
+										className='result'
+										onMouseEnter={() => setHoveredCell({ multiplier, num })}
+										onMouseLeave={() => setHoveredCell(null)}
+									>
+										{hoveredCell &&
+										hoveredCell.multiplier === multiplier &&
+										hoveredCell.num === num
+											? multiplier * num
+											: ""}
+									</td>
+								</tr>
 							))}
-						</tr>
-					))}
-				</tbody>
-			</table>
+						</tbody>
+					</table>
+				</div>
+			))}
 		</div>
 	);
 }
